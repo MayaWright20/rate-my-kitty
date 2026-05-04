9907989c2ed1d6c9ee2b993b1ba504686d085a9b
mayagwright20 <mayagwright20@hotmail.com>
2026-05-05 00:14:57 +0100

    Add Image picker

--- components/upload/image-uploader.tsx
+++ components/upload/image-uploader.tsx
@@ -1,43 +1,113 @@
 import { COLORS } from "@/constants/colors";
 import Ionicons from "@expo/vector-icons/Ionicons";
-import { Pressable, StyleSheet, Text } from "react-native";
+import { Image } from "expo-image";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { Alert, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function ImageUploader() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      Alert.alert(
+        "Permission required",
+        "Permission to access the media library is required.",
+      );
+      return;
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
-    <Pressable style={styles.container}>
-      <Ionicons
-        style={styles.icon}
-        name={"camera"}
-        color={COLORS.PURPLE[3]}
-        size={70}
-      />
-      <Text style={styles.title}>Tap to upload</Text>
-      <Text style={styles.subheading}>Pick your best cat picture!</Text>
-    </Pressable>
+    <>
+      {image && <Image source={{ uri: image }} style={styles.image} />}
+      <Pressable
+        style={
+          image
+            ? [styles.container, styles.containerSmall]
+            : [styles.container, styles.containerLarge]
+        }
+        onPress={pickImage}
+      >
+        <Ionicons
+          style={[styles.icon, image && styles.iconSmall]}
+          name={"camera"}
+          color={COLORS.PURPLE[3]}
+          size={image ? 50 : 70}
+        />
+        <View style={!image && styles.textWrapper}>
+          <Text style={styles.title}>
+            {image ? "Edit photo" : "Tap to upload"}
+          </Text>
+          <Text>{"Pick your best cat picture!"}</Text>
+        </View>
+      </Pressable>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: COLORS.PURPLE[0],
-    width: "90%",
     borderStyle: "dotted",
     borderWidth: 5,
     borderColor: COLORS.PURPLE[1],
-    aspectRatio: 2 / 1.5,
     margin: 15,
     borderRadius: 15,
     alignItems: "center",
     justifyContent: "center",
   },
+  containerSmall: {
+    flexDirection: "row",
+    aspectRatio: 2 / 0.5,
+  },
+  containerLarge: {
+    width: "90%",
+    aspectRatio: 2 / 1.5,
+  },
   icon: {
     marginBottom: 5,
+  },
+  iconSmall: {
+    marginRight: 15,
   },
   title: {
     color: COLORS.PURPLE[3],
     fontWeight: "bold",
+  },
+  titleLarge: {
     fontSize: 25,
     marginBottom: 15,
   },
-  subheading: {},
+  titleSmall: {
+    fontSize: 15,
+    marginBottom: 10,
+  },
+  textWrapper: {
+    alignItems: "center",
+  },
+  image: {
+    aspectRatio: 1 / 1,
+    width: "90%",
+    marginTop: 25,
+    borderRadius: 15,
+    borderWidth: 5,
+    borderColor: COLORS.PURPLE[1],
+  },
 });

a2aef226dec98d3f2ba89da394912b8985ceb4ce
mayagwright20 <mayagwright20@hotmail.com>
2026-05-04 18:29:00 +0100

    Add initial layout of upload screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,26 @@
-import { Stack } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="upload/index"
+        options={{
+          title: "upload",
+          tabBarIcon: ({ focused }) => (
+            <Ionicons
+              name={"add-sharp"}
+              color={focused ? "red" : "grey"}
+              size={24}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }

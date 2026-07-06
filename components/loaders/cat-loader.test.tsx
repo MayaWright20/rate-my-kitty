7bcfffed44b526d948fb80d005fcad11a306aa5e
mayagwright20 <mayagwright20@hotmail.com>
2026-07-06 23:59:22 +0100

    Add cat loader test

---
+++ components/loaders/cat-loader.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from "@testing-library/react-native";
+
+import CatLoader from "./cat-loader";
+
+test("should render catloader", async () => {
+  await render(<CatLoader />);
+
+  const result = screen.getByTestId("cat-loader");
+
+  expect(result).toBeOnTheScreen();
+});

41b7466f9de2c70346e03404c6f172ec6d1a5a7b
mayagwright20 <mayagwright20@hotmail.com>
2026-07-07 09:17:04 +0100

    Update live doc with how to see live tests

--- components/buttons/circular-btn.test.tsx
+++ components/buttons/circular-btn.test.tsx
@@ -1,103 +1,103 @@
-// import { render, screen, userEvent } from "@testing-library/react-native";
-// import { StyleSheet } from "react-native";
+import { render, screen, userEvent } from "@testing-library/react-native";
+import { StyleSheet } from "react-native";
 
-// import { IsScreenPortraitContext } from "@/context/screen-orientation-context";
+import { IsScreenPortraitContext } from "@/context/screen-orientation-context";
 
-// import CircularBTN from "./circular-btn";
+import CircularBTN from "./circular-btn";
 
-// function renderWithContext(ui: React.ReactElement, isPortrait: boolean) {
-//   return render(
-//     <IsScreenPortraitContext.Provider value={isPortrait}>
-//       {ui}
-//     </IsScreenPortraitContext.Provider>
-//   );
-// }
+function renderWithContext(ui: React.ReactElement, isPortrait: boolean) {
+  return render(
+    <IsScreenPortraitContext.Provider value={isPortrait}>
+      {ui}
+    </IsScreenPortraitContext.Provider>
+  );
+}
 
-// test("should render the title when provided", async () => {
-//   await render(<CircularBTN title="VOTE" />);
+test("should render the title when provided", async () => {
+  await render(<CircularBTN title="VOTE" />);
 
-//   expect(await screen.findByText("VOTE")).toBeOnTheScreen();
-// });
+  expect(await screen.findByText("VOTE")).toBeOnTheScreen();
+});
 
-// test("should render without crashing when no props are provided", async () => {
-//   await render(<CircularBTN />);
+test("should render without crashing when no props are provided", async () => {
+  await render(<CircularBTN />);
 
-//   expect(screen.getByTestId("circular-btn")).toBeOnTheScreen();
-// });
+  expect(screen.getByTestId("circular-btn")).toBeOnTheScreen();
+});
 
-// test("should render with custom title color if titleColor prop provided", async () => {
-//   await render(<CircularBTN title="VOTE" titleColor={"pink"} />);
+test("should render with custom title color if titleColor prop provided", async () => {
+  await render(<CircularBTN title="VOTE" titleColor={"pink"} />);
 
-//   const title = screen.getByText("VOTE");
-//   const flatStyle = StyleSheet.flatten(title.props.style);
+  const title = screen.getByText("VOTE");
+  const flatStyle = StyleSheet.flatten(title.props.style);
 
-//   expect(flatStyle.color).toBe("pink");
-// });
+  expect(flatStyle.color).toBe("pink");
+});
 
-// test("should render with default title color if titleColor prop is not provided", async () => {
-//   await render(<CircularBTN title="VOTE" />);
+test("should render with default title color if titleColor prop is not provided", async () => {
+  await render(<CircularBTN title="VOTE" />);
 
-//   const title = screen.getByText("VOTE");
-//   const flatStyle = StyleSheet.flatten(title.props.style);
+  const title = screen.getByText("VOTE");
+  const flatStyle = StyleSheet.flatten(title.props.style);
 
-//   expect(flatStyle.color).toBe("#4B4B57");
-// });
+  expect(flatStyle.color).toBe("#4B4B57");
+});
 
-// test("should render with default title color if titleColor prop is undefined", async () => {
-//   await render(<CircularBTN title="VOTE" titleColor={undefined} />);
+test("should render with default title color if titleColor prop is undefined", async () => {
+  await render(<CircularBTN title="VOTE" titleColor={undefined} />);
 
-//   const title = screen.getByText("VOTE");
-//   const flatStyle = StyleSheet.flatten(title.props.style);
+  const title = screen.getByText("VOTE");
+  const flatStyle = StyleSheet.flatten(title.props.style);
 
-//   expect(flatStyle.color).toBe("#4B4B57");
-// });
+  expect(flatStyle.color).toBe("#4B4B57");
+});
 
-// test("should render with an icon if icon is provided", async () => {
-//   await render(
-//     <CircularBTN icon={{ name: "heart", size: 24, color: "red" }} />
-//   );
+test("should render with an icon if icon is provided", async () => {
+  await render(
+    <CircularBTN icon={{ name: "heart", size: 24, color: "red" }} />
+  );
 
-//   const icon = screen.getByTestId("circular-btn-icon-wrapper");
+  const icon = screen.getByTestId("circular-btn-icon-wrapper");
 
-//   expect(icon).toBeOnTheScreen();
-// });
+  expect(icon).toBeOnTheScreen();
+});
 
-// test("should render with custom background color if backgroundColor prop provided", async () => {
-//   await render(<CircularBTN title="VOTE" backgroundColor={"blue"} />);
+test("should render with custom background color if backgroundColor prop provided", async () => {
+  await render(<CircularBTN title="VOTE" backgroundColor={"blue"} />);
 
-//   const iconWrapper = screen.getByTestId("circular-btn-icon-wrapper");
-//   const flatStyle = StyleSheet.flatten(iconWrapper.props.style);
+  const iconWrapper = screen.getByTestId("circular-btn-icon-wrapper");
+  const flatStyle = StyleSheet.flatten(iconWrapper.props.style);
 
-//   expect(flatStyle.backgroundColor).toBe("blue");
-// });
+  expect(flatStyle.backgroundColor).toBe("blue");
+});
 
-// test("should call onPress when the button is pressed", async () => {
-//   const mockFn = jest.fn();
+test("should call onPress when the button is pressed", async () => {
+  const mockFn = jest.fn();
 
-//   await render(<CircularBTN onPress={mockFn} />);
+  await render(<CircularBTN onPress={mockFn} />);
 
-//   const circleBtn = screen.getByTestId("circular-btn"); // cant get id until the component is rendered await render(<CircularBTN onPress={mockFn} />);
+  const circleBtn = screen.getByTestId("circular-btn"); // cant get id until the component is rendered await render(<CircularBTN onPress={mockFn} />);
 
-//   const user = userEvent.setup();
-//   await user.press(circleBtn);
+  const user = userEvent.setup();
+  await user.press(circleBtn);
 
-//   expect(mockFn).toHaveBeenCalled();
-// });
+  expect(mockFn).toHaveBeenCalled();
+});
 
-// test("should use portrait width of 70% when in portrait mode", async () => {
-//   await renderWithContext(<CircularBTN />, true);
+test("should use portrait width of 70% when in portrait mode", async () => {
+  await renderWithContext(<CircularBTN />, true);
 
-//   const iconWrapper = screen.getByTestId("circular-btn-icon-wrapper");
-//   const flatStyle = StyleSheet.flatten(iconWrapper.props.style);
+  const iconWrapper = screen.getByTestId("circular-btn-icon-wrapper");
+  const flatStyle = StyleSheet.flatten(iconWrapper.props.style);
 
-//   expect(flatStyle.width).toBe("70%");
-// });
+  expect(flatStyle.width).toBe("70%");
+});
 
-// test("should use landscape width of 40% when in landscape mode", async () => {
-//   await renderWithContext(<CircularBTN />, false);
+test("should use landscape width of 40% when in landscape mode", async () => {
+  await renderWithContext(<CircularBTN />, false);
 
-//   const iconWrapper = screen.getByTestId("circular-btn-icon-wrapper");
-//   const flatStyle = StyleSheet.flatten(iconWrapper.props.style);
+  const iconWrapper = screen.getByTestId("circular-btn-icon-wrapper");
+  const flatStyle = StyleSheet.flatten(iconWrapper.props.style);
 
-//   expect(flatStyle.width).toBe("40%");
-// });
+  expect(flatStyle.width).toBe("40%");
+});
